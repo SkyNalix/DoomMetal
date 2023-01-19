780f0346ce1756688958fe04a408dee5df23833c
Tazouev Arbi <tazouev.arbi@gmail.com>
2023-01-19 18:05:41 +0100

    ajout des ombres sur les murs

--- bin/Drawer3D.ml
+++ bin/Drawer3D.ml
@@ -21,9 +21,13 @@ let drawRay windows_info level ray i =
         windows_info.height/2 - rect_height/2
     ) in
 
+    let (r,g,b) = Drawer2D.color_of_wall 
+        level.plot.(int_of_float ray.touched_pos.y).(int_of_float ray.touched_pos.x) in
+    let r = if r = 0 then 0 else r + int_of_float (250. /. ray.distance) in
+    let g = if g = 0 then 0 else g + int_of_float (250. /. ray.distance) in
+    let b = if b = 0 then 0 else b + int_of_float (250. /. ray.distance) in
     A.fill_rectangle windows_info.area3D
-        ~color:(Draw.opaque (Drawer2D.color_of_wall 
-                level.plot.(int_of_float ray.touched_pos.y).(int_of_float ray.touched_pos.x)) )
+        ~color:(Draw.opaque (r,g,b))
         (* ~color:(Draw.random_color ()) *)
         ~w:win_step
         ~h:rect_height
