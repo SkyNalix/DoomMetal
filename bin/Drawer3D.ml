be79fbda1e40c4a763b98434f6e6f805dd851d29
Tazouev Arbi <tazouev.arbi@gmail.com>
2023-01-20 14:01:33 +0100

    fix affichage avec les ombres

--- bin/Drawer3D.ml
+++ bin/Drawer3D.ml
@@ -11,7 +11,6 @@ let drawRay windows_info level ray i =
     
     if not (ray.rayTouched) then (failwith "ray not touched") else
 
-    (* let win_step = windows_info.width / (ray.angle_max - ray.angle_min) in  *)
     let win_step = windows_info.width / (ray.angle_max - ray.angle_min) in 
     let rect_height = (int_of_float ( float_of_int(windows_info.height) /. ray.distance)) in
 
@@ -23,12 +22,11 @@ let drawRay windows_info level ray i =
 
     let (r,g,b) = Drawer2D.color_of_wall 
         level.plot.(int_of_float ray.touched_pos.y).(int_of_float ray.touched_pos.x) in
-    let r = if r = 0 then 0 else r + int_of_float (250. /. ray.distance) in
-    let g = if g = 0 then 0 else g + int_of_float (250. /. ray.distance) in
-    let b = if b = 0 then 0 else b + int_of_float (250. /. ray.distance) in
+    let r = if r = 0 then 0 else max 10 r - int_of_float (30. *. ray.distance) in
+    let g = if g = 0 then 0 else max 10 g - int_of_float (30. *. ray.distance) in
+    let b = if b = 0 then 0 else max 10 b - int_of_float (30. *. ray.distance) in
     A.fill_rectangle windows_info.area3D
         ~color:(Draw.opaque (r,g,b))
-        (* ~color:(Draw.random_color ()) *)
         ~w:win_step
         ~h:rect_height
         rec_start;
