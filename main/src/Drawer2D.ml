ca7b6a06cb9ff25b47890bd60c031ddf1cb8bf40
Tazouev Arbi <tazouev.arbi@gmail.com>
2023-05-17 18:38:11 +0200

    nettoyage du drawer2D

--- main/src/Drawer2D.ml
+++ main/src/Drawer2D.ml
@@ -13,18 +13,15 @@ let color_of_wall = function
 
 let render windows_info level rays = 
     let plot = level.map.plot in
-    let view_angle = level.player.entity.view_angle in
-    let player_pos = level.player.entity.pos in
-
     let block_width = (windows_info.drawer2D_width / level.map.width) in
     let block_height = (windows_info.drawer2D_height / level.map.height) in
 
     let drawRay ray = 
         
         if not ray.rayTouched then () else
-
-        let player_posX = int_of_float (float_of_int block_width *. player_pos.x) in
-        let player_posY = int_of_float (float_of_int block_height *. player_pos.y) in    
+        let pos = level.player.entity.pos in
+        let posX = int_of_float (float_of_int block_width *. pos.x) in
+        let posY = int_of_float (float_of_int block_height *. pos.y) in    
         
         Sdlrender.set_draw_color windows_info.render ~rgb:blue ~a:255 ;
         let rect = Sdlrect.make 
@@ -33,18 +30,13 @@ let render windows_info level rays =
                 ~dims:(block_width, block_height) in
         Sdlrender.fill_rect windows_info.render rect;
 
-        let color = (
-            if view_angle -. 5. <  ray.angle && ray.angle < view_angle +. 5. then
-                red
-            else grey 
-        ) in
-        Sdlrender.set_draw_color windows_info.render ~rgb:color ~a:255 ;
+        Sdlrender.set_draw_color windows_info.render ~rgb:grey ~a:255 ;
         Sdlrender.draw_line2 
             windows_info.render
             ~p1:(
-                player_posX + int_of_float (ray.angle_vec.x *. 999.), 
-                player_posY + int_of_float (ray.angle_vec.y *. 999.))
-            ~p2:(player_posX, player_posY);
+                posX + int_of_float (ray.angle_vec.x *. 999.), 
+                posY + int_of_float (ray.angle_vec.y *. 999.))
+            ~p2:(posX, posY);
     in
 
     let rec drawPlot y x : unit =
@@ -60,32 +52,30 @@ let render windows_info level rays =
         if y >= level.map.height then () else drawPlot y x
     in
 
-    let drawLiving pos color : unit =
+    let drawEntity entity color : unit =
         let sz = 10 in
-        let x = int_of_float (pos.x *. float_of_int block_width) - sz/2 in
-        let y = int_of_float (pos.y *. float_of_int block_height) - sz/2 in
+        let x = int_of_float (entity.pos.x *. float_of_int block_width) - sz/2 in
+        let y = int_of_float (entity.pos.y *. float_of_int block_height) - sz/2 in
         Sdlrender.set_draw_color windows_info.render ~rgb:color ~a:255 ;
         let rect = Sdlrect.make 
                 ~pos:(x, y)
                 ~dims:(sz, sz) in
-        Sdlrender.fill_rect windows_info.render rect
-    in
+        Sdlrender.fill_rect windows_info.render rect;
 
-    drawPlot 0 0 ;
-    List.iter (fun ray -> drawRay ray) rays;
-    drawLiving player_pos white;
-    List.iter (fun enemy -> drawLiving enemy.pos orange) level.enemies;
-
-    List.iter (fun enemy ->
-        let enemy_posX = int_of_float (float_of_int block_width *. enemy.pos.x) in
-        let enemy_posY = int_of_float (float_of_int block_height *. enemy.pos.y) in    
-        let angle_vec = Common.angleAsVec enemy.view_angle in
+        let entity_posX = int_of_float (float_of_int block_width *. entity.pos.x) in
+        let entity_posY = int_of_float (float_of_int block_height *. entity.pos.y) in    
+        let angle_vec = Common.angleAsVec entity.view_angle in
         Sdlrender.set_draw_color windows_info.render ~rgb:blue ~a:255 ;
         Sdlrender.draw_line2 
             windows_info.render
             ~p1:(
-                enemy_posX + int_of_float (angle_vec.x *. 100.), 
-                enemy_posY + int_of_float (angle_vec.y *. 100.))
-            ~p2:(enemy_posX, enemy_posY)
-        ) level.enemies;
-    ();;
+                entity_posX + int_of_float (angle_vec.x *. 100.), 
+                entity_posY + int_of_float (angle_vec.y *. 100.))
+            ~p2:(entity_posX, entity_posY)
+    in
+
+    drawPlot 0 0 ;
+    List.iter (fun ray -> drawRay ray) rays;
+    drawEntity level.player.entity white;
+    List.iter (fun enemy -> drawEntity enemy orange) level.enemies;
+();;
